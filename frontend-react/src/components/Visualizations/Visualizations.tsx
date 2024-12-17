2f0a2663cb57adb9132aca0a4f1b4dd2e843234b
Samuel Ojuri <samuel.ojuri@gmail.com>
2024-12-17 20:43:44 +0000

    added cors update

--- frontend-react/src/components/Visualizations/Visualizations.tsx
+++ frontend-react/src/components/Visualizations/Visualizations.tsx
@@ -84,7 +84,7 @@ const Visualizations: React.FC = () => {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
         },
-        mode: 'cors',
+        mode: 'no-cors',
         credentials: 'include',
         body: JSON.stringify({
           query: selectedViz === "Create a Visualisation" ? customQuery : vizOptions[selectedViz]
