b797f987dc1527d40ea319ebfaa19a2f31407631
Samuel Ojuri <samuel.ojuri@gmail.com>
2025-02-04 06:07:58 +0000

    Updated chat agent

--- backend/chat_agent.py
+++ backend/chat_agent.py
@@ -72,8 +72,8 @@ except Exception as e:
 tools = toolkit.get_tools()
 
 
-REACT_TEMPLATE = """You are an agent designed to interact with a SQL database.
-Given an input question, create a syntactically correct SQLite query to run, then look at the results of the query and return the answer.
+REACT_TEMPLATE = """You are an agent designed to interact with a PostgreSQL database.
+Given an input question, create a syntactically correct PostgreSQL query to run, then look at the results of the query and return the answer.
 Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
 You can order the results by a relevant column to return the most interesting examples in the database.
 Never query for all the columns from a specific table, only ask for the relevant columns given the question.
